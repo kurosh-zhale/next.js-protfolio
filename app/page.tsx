76a9d2da9ccbebe2bf86ba15115d119c5a3c0633
Kurosh Zhalerajabi <kjreem.imagine@gmail.com>
2025-04-24 19:54:09 +0200

    [chng] modified some other components

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
 import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+
+type TestType = { title: string; content: string; id: `item-${number}` };
 
 export default async function Home() {
   return (
     <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+      <main
+        className="relative bg-black-100 flex
+       justify-centeritems-center flex-col 
+       overflow-hidden mx-auto sm:px-10 px-5"
+      >
+        <div className="max-w-7xl w-full">
+          <Hero />
+        </div>
       </main>
     </>
   );
